db2e42536b8ea781695d7658fa01461af0f373c9
Sujith K <sujithk1910@gmail.com>
2021-06-08 17:01:19 +0530

    added imp=high for pic04

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ let options = {
         if (path.split("/")[path.split("/").length-1] == 'index.html') {
             res.set('x-timestamp', Date.now())
             // res.set('Link', '<https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,700,900>; rel="preload"; as="style"')
-            res.set('Link', '</images/pic12.jpg>; rel="preload";')
+            res.set('Link', '</images/pic04.jpg>; rel="preload"; importance="high"')
             // res.links({
                 // preload: "https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,700,900; as=style",
                 // preload: "http://localhost:3000/images/pic12.jpg; importance=high"
