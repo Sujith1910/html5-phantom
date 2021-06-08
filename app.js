9d119e127a47a43105d8de710f460268472b3cae
Sujith K <sujithk1910@gmail.com>
2021-06-08 15:29:48 +0530

    created node.js server

---
+++ app.js
@@ -0,0 +1,44 @@
+const express=require('express');
+const path = require('path');
+const http = require('http');
+
+const app=express();
+
+const staticPath = path.join(__dirname, './assets')
+
+let options = {
+    setHeaders: function (res, path, stat) {
+        // res.set('x-timestamp', Date.now())
+        res.set('x-timestamp', Date.now())
+        res.set('Link', '<https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,700,900>; rel="preload"')
+        res.set('Link', '<http://localhost:3000/images/pic12.jpg>; rel="preload"; importance=high')
+        // res.links({
+            // preload: "https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,700,900; as=style",
+            // preload: "http://localhost:3000/images/pic12.jpg; importance=high"
+            // prefetch: "https://twitter.com"
+            // preload: "https://www.twitter.com"
+        // })
+      }
+}
+
+app.use(express.static(staticPath, options));
+
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    // console.log(req.hostname);
+    // res.send("Hello world!");
+    // res.links({
+    //     preload: "https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,700,900; importance=high ",
+    //     prefetch: "https://twitter.com"
+    // })
+    // res.sendFile(path.join(__dirname, './index.html'));
+    
+})
+
+const PORT=process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log('Listening on port 3000!');
+})
